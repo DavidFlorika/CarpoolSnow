5dbd4756a6dcd477be8a96cac8852d12ad564647
Luotanw <161969273+Luotanw@users.noreply.github.com>
2025-06-20 18:02:09 -0700

    basic update on backend

--- app.py
+++ app.py
@@ -1,23 +1,29 @@
-# app.py
-from flask import Flask, render_template
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from config import Config
 
-app = Flask(__name__)
+db = SQLAlchemy()
+login_manager = LoginManager()
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-@app.route('/account')
-def account():
-    return render_template('account.html')
+    db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
 
-@app.route('/mycarpool')
-def my_carpool():
-    return render_template('my_carpool.html')
+    from routes.auth import auth_bp
+    from routes.carpool import carpool_bp
+    from routes.main import main_bp
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(carpool_bp)
+    app.register_blueprint(main_bp)
+
+    return app
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
